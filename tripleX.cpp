eb841bb84b90087ab4e1be9314ead2d97ba6680f
Hannah Preisinger <55310683+hpreisinger@users.noreply.github.com>
2020-01-12 17:37:56 -0800

    added intro message

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a peerless thief, renowed throughout the land for your ability to quietly obtain things which are not yours.";
+    std::cout << std::endl;
+    std::cout << "You have broken into the dungeons of a strange old magician who has chosen to protect his valuables with a series of numerical riddles.";
+    std::cout << std::endl;
+    std::cout << "You must enter the correct codes in order to progress through the dungeon and claim your prize.";
+    std::cout << std::endl;
+    std::cout << "Now get to it! You've a reputation to maintain...";
+    std::cout << std::endl;
     return 0;
 }
