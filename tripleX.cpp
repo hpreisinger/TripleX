1b915b963ceb03cf527a709a9815cdb7580dce41
Hannah Preisinger <55310683+hpreisinger@users.noreply.github.com>
2020-01-12 18:12:25 -0800

    adding player hints w/ sum and product

--- tripleX.cpp
+++ tripleX.cpp
@@ -2,20 +2,27 @@
 
 int main()
 {
+    //prints intro message
     std::cout << "You are a peerless thief, renowed throughout the land for your ability to quietly obtain things which are not yours." << std::endl;
     std::cout << "You have broken into the dungeons of a strange old magician who has chosen to protect his valuables with a series of numerical riddles." << std::endl;
     std::cout << "You must enter the correct codes in order to progress through the dungeon and claim your prize." << std::endl;
     std::cout << "Now get to it! You've a reputation to maintain..." << std::endl;
 
-    const int a = 4;
-    const int b = 7;
-    const int c = 9;
+    //declares three code numbers
+    const int CodeA = 4;
+    const int CodeB = 7;
+    const int CodeC = 9;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    // prints sum and product
+    std:: cout << std::endl;
+    std::cout << "+ There are three numbers in the code." << std:: endl;
+    std::cout << "+ The three numbers add up to " << CodeSum << "." << std::endl;
+    std::cout << "+ Multiplying the three numbers gives you " << CodeProduct << "." << std::endl;
+
+    int PlayerGuess;
 
     return 0;
 }
